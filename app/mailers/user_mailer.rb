f2a8dfabcaf2ba92d9007a575b6ed9b1b4aeb1e8
qasimir <h.coulson@outlook.com>
2017-02-16 11:36:22 +1300

    intermediate commit

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,10 @@
 class UserMailer < ApplicationMailer
+	default from: 'address@example.com'
+
+	def welcome_email(user)
+		@user = user
+		@url = 'spiffjigg'
+		mail(to: @user.email,subject: 'this is a subject title')
+
+	end
 end
